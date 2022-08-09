3fc130d8dda04492b8641a56b0cac62285511245
Grace Wang <t4_23@yahoo.com.tw>
2022-08-09 16:13:29 +0800

    Update 2022-08-09T08:13:26.492Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,24 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+$(document).ready(function () {
+  $('.reply-btn').click(function (e) {
+    $('.reply').toggleClass('d-none');
+    $('.editor').toggleClass('d-none');
+    $('.link-reply-btn').toggleClass('d-none');
+  });
+});
+$(document).ready(function () {
+  $('.cancel-btn').click(function (e) {
+    $('.reply').toggleClass('d-none');
+    $('.editor').toggleClass('d-none');
+    $('.link-reply-btn').toggleClass('d-none');
+  });
+});
+$(document).ready(function () {
+  $('.edit-btn').click(function (e) {
+    $('.reply').toggleClass('d-none');
+    $('.editor').toggleClass('d-none');
+    $('.link-reply-btn').toggleClass('d-none');
+  });
 });
 //# sourceMappingURL=all.js.map
