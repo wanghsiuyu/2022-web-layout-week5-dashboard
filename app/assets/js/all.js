20e8c5b95c740243c7d39113f09af0a7e1af7567
Grace Wang <t4_23@yahoo.com.tw>
2022-08-09 16:15:08 +0800

    add index.html

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,21 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+$(document).ready(function () {
+  $('.reply-btn').click(function (e) {
+    $('.reply').toggleClass('d-none');
+    $('.editor').toggleClass('d-none');
+    $('.link-reply-btn').toggleClass('d-none');
+  });
+});
+$(document).ready(function () {
+  $('.cancel-btn').click(function (e) {
+    $('.reply').toggleClass('d-none');
+    $('.editor').toggleClass('d-none');
+    $('.link-reply-btn').toggleClass('d-none');
+  });
+});
+$(document).ready(function () {
+  $('.edit-btn').click(function (e) {
+    $('.reply').toggleClass('d-none');
+    $('.editor').toggleClass('d-none');
+    $('.link-reply-btn').toggleClass('d-none');
+  });
 });
